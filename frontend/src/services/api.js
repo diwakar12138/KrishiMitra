86c93072a79ca3b6c89eb39fd63ee50f2b7318e4
diwakar12138 <diwakargautam823@gmail.com>
2026-09-17 20:16:04 +0530

    deploying step -1 frontend vite url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+const BASE_URL = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL}/api/v1` 
+  : "http://localhost:5000/api/v1";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
